4bc3264e62a2bf63a6d6cfe01a1bcbbd28043200
fifth_light <liu-yu-jie@foxmail.com>
2026-03-23 16:34:02 +0800

    [TouchController] 为 API 加入 @NullMarked 注解

---
+++ touchcontroller/api/v1/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2026 fifth_light
+ */
+
+@NullMarked
+package top.fifthlight.touchcontroller.api.v1;
+
+import org.jspecify.annotations.NullMarked;
